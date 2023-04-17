45065e5ce76c324d422116fe4d94068fd51c7550
dtimofeev <td090833@gmail.com>
2023-04-17 20:00:43 +0300

    Implemented `randm` function

--- Lab3.1/main.c
+++ Lab3.1/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+double** randm (int n) {
+  srand(2220);
+  double** matrix = (double**) malloc(sizeof(double*) * n);
+  for (int i = 0; i < n; i++) {
+    matrix[i] = (double*) malloc(sizeof(double)*n);
+  }
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j <n; j++) {
+      matrix[i][j] = (double)(rand() *2.0)/ (double)RAND_MAX;
+    }
+  }
+  return matrix;
 
+}
 
