f5131a297eeb3a1fed772a43ddf16da8584420ea
dtimofeev <td090833@gmail.com>
2023-04-22 15:33:13 +0300

    Added circle parameters

--- Lab3.1/main.c
+++ Lab3.1/main.c
@@ -1,27 +1,171 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
-double ** createMatrix (int n) {
-  double** matrix = (double**) malloc(sizeof(double*) * n);
+#include <math.h>
+
+#define vertices 12
+#define IDC_BUTTON 3456
+#define IDC_BUTTON2 3457
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+char ProgName[] = "Lab 3";
+
+struct coordinates {
+    double nx[vertices];
+    double ny[vertices];
+    double loopX[vertices];
+    double loopY[vertices];
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow) {
+  WNDCLASS w;
+  w.lpszClassName = ProgName;
+  w.hInstance = hInstance;
+  w.lpfnWndProc = WndProc;
+  w.hCursor = LoadCursor(NULL, IDC_ARROW);
+  w.hIcon = 0;
+  w.lpszMenuName = 0;
+  w.hbrBackground = WHITE_BRUSH;
+  w.style = CS_HREDRAW | CS_VREDRAW;
+  w.cbClsExtra = 0;
+  w.cbWndExtra = 0;
+  if (!RegisterClass(&w)) {
+    return 0;
+  }
+  HWND hWnd;
+  MSG lpMsg;
+  hWnd = CreateWindow(ProgName,
+                      L"Lab 3. Made by Daniil Timofeev",
+                      WS_OVERLAPPEDWINDOW,
+                      100,
+                      100,
+                      1000,
+                      700,
+                      (HWND) NULL,
+                      (HMENU) NULL,
+                      (HINSTANCE) hInstance,
+                      (HINSTANCE) NULL);
+  ShowWindow(hWnd, nCmdShow);
+  while (GetMessage(&lpMsg, hWnd, 0, 0)) {
+    TranslateMessage(&lpMsg);
+    DispatchMessage(&lpMsg);
+  }
+  return (lpMsg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT messg, WPARAM wParam, LPARAM lParam) {
+  HDC hdc;
+  PAINTSTRUCT ps;
+  HWND hwndButton_direct;
+  HWND hwndButton_undirect;
+  int flag = 1;
+  switch (messg) {
+    case WM_CREATE: {
+      hwndButton_direct = CreateWindow(
+              L"BUTTON",
+              L"Directed graph",
+              WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+              700,
+              50,
+              160,
+              50,
+              hWnd,
+              (HMENU) IDC_BUTTON,
+              (HINSTANCE) GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+              NULL);
+      hwndButton_undirect = CreateWindow(
+              L"BUTTON",
+              L"Undirected graph",
+              WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+              700,
+              125,
+              160,
+              50,
+              hWnd,
+              (HMENU) IDC_BUTTON2,
+              (HINSTANCE) GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+              NULL);
+      return 0;
+    }
+    case WM_COMMAND: {
+      switch (LOWORD(wParam)) {
+
+        case IDC_BUTTON:
+          flag = 0;
+          InvalidateRect(hWnd, NULL, FALSE);
+          break;
+
+        case IDC_BUTTON2:
+          flag = 1;
+          InvalidateRect(hWnd, NULL, FALSE);
+          break;
+      }
+    }
+    case WM_PAINT :
+      hdc = BeginPaint(hWnd, &ps);
+      SetGraphicsMode(hdc, GM_ADVANCED);
+      HPEN BPen = CreatePen(PS_SOLID, 2, RGB(50, 0, 255));
+      HPEN KPen = CreatePen(PS_SOLID, 1, RGB(20, 20, 5));
+      HPEN GPen = CreatePen(PS_SOLID, 2, RGB(128, 0, 128));
+      HPEN NoPen = CreatePen(PS_NULL, 0, RGB(0, 0, 0));
+      SelectObject(hdc, NoPen);
+      Rectangle(hdc, 0, 0, 670, 700);
+
+
+      wchar_t *nn[vertices] = {
+              L"1", L"2", L"3", L"4", L"5", L"6",
+              L"7", L"8", L"9", L"10", L"11", L"12"
+      };
+
+      struct coordinates coordinates;
+
+      double circleRadius = 300;
+      double vertexRadius = circleRadius / 10;
+
+      double loopRadius = vertexRadius;
+
+      double circleCenterX = 450;
+      double circleCenterY = 450;
+
+      double angleAlpha = 2.0 * M_PI / (double) vertices;
+      for (int i = 0; i < vertices; i++) {
+        double sinAlpha = sin(angleAlpha * (double) i);
+        double cosAlpha = cos(angleAlpha * (double) i);
+        coordinates.nx[i] = circleCenterX + circleRadius * sinAlpha;
+        coordinates.ny[i] = circleCenterY - circleRadius * cosAlpha;
+        coordinates.loopX[i] = circleCenterX + (circleRadius + loopRadius) * sinAlpha;
+        coordinates.loopY[i] = circleCenterY - (circleRadius + loopRadius) * cosAlpha;
+      }
+
+
+  }
+}
+
+
+double **createMatrix(int n) {
+  double **matrix = (double **) malloc(sizeof(double *) * n);
   for (int i = 0; i < n; i++) {
-    matrix[i] = (double*) malloc(sizeof(double) * n);
+    matrix[i] = (double *) malloc(sizeof(double) * n);
   }
   return matrix;
 }
-double** randm (int n) {
+
+double **randm(int n) {
   srand(2220);
-  double** matrix = createMatrix(n);
+  double **matrix = createMatrix(n);
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j <n; j++) {
-      matrix[i][j] = (double)(rand() *2.0)/ (double)RAND_MAX;
+    for (int j = 0; j < n; j++) {
+      matrix[i][j] = (double) (rand() * 2.0) / (double) RAND_MAX;
     }
   }
   return matrix;
 
 }
-double **mulmr (double coef, double** matrix, int n) {
-  for (int i = 0; i <n; i++) {
-    for (int j = 0; j <n; j++) {
+
+double **mulmr(double coef, double **matrix, int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
       matrix[i][j] *= coef;
       matrix[i][j] = matrix[i][j] < 1 ? 0 : 1;
     }
@@ -30,23 +174,23 @@ double **mulmr (double coef, double** matrix, int n) {
 }
 
 
-double** symmetricMatrix(int n) {
-  double** symmetric = createMatrix(n);
-  for (int i = 0; i <n; i++) {
-    for (int j = 0; j <n; j++) {
+double **symmetricMatrix(int n) {
+  double **symmetric = createMatrix(n);
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
       if (symmetric[i][j] == 1) symmetric[j][i] = 1;
     }
   }
   return symmetric;
 }
-void freeMatrix(double** matrix, int n) {
-  for (int i = 0; i <n; ++i) {
+
+void freeMatrix(double **matrix, int n) {
+  for (int i = 0; i < n; ++i) {
     free(matrix[i]);
   }
   free(matrix);
 }
 
-int main ()
-{
+int main() {
   printf("Hello");
 }
